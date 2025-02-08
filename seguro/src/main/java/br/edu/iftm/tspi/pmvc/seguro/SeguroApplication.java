0adfc6aaff4b577003fbf36772eb49b7419fc1f4
Fernandofgs91 <162770609+Fernandofgs91@users.noreply.github.com>
2025-02-08 17:05:57 -0300

    Update SeguroApplication.java

--- seguro/src/main/java/br/edu/iftm/tspi/pmvc/seguro/SeguroApplication.java
+++ seguro/src/main/java/br/edu/iftm/tspi/pmvc/seguro/SeguroApplication.java
@@ -11,3 +11,29 @@ public class SeguroApplication {
 	}
 
 }
+@Configuration
+
+public static class CorsConfiguracao {
+
+@Bean
+
+public WebMvcConfigurer corsConfigurer() {
+
+	return new WebMvcConfigurer() {
+
+	@Override
+
+public void addCorsMappings(CorsRegistry registry) {
+
+	registry.addMapping("/**")
+
+	.allowedMethods("HEAD","GET","PUT","POST","DELETE","PATCH");
+
+				}
+
+			};
+
+		}
+
+	}
+}
